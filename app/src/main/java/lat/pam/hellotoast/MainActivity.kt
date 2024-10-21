9bfb7b741ba8d01763096cf9c38c9d6beb21fedc
aldyrialdy <aldyrialdyatmadja@gmail.com>
2024-10-21 17:27:41 +0700

    Remove live data

--- app/src/main/java/lat/pam/hellotoast/MainActivity.kt
+++ app/src/main/java/lat/pam/hellotoast/MainActivity.kt
@@ -4,22 +4,17 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
-    private val model: NameViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,22 +25,10 @@ class MainActivity : AppCompatActivity() {
         val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
         val buttonBrowser = findViewById<Button>(R.id.button_browser)
 
-        // Create the observer which updates the UI.
-        val nameObserver = Observer<Int> { newName ->
-            // Update the UI, in this case, a TextView.
-            mShowCount.text = newName.toString()
-        }
-
-        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
-        model.currentName.observe(this, nameObserver)
-
-        mCount = model.currentName.value?:0
         buttonCountUp.setOnClickListener(View. OnClickListener {
-//            Log.d("Nilai variabel",mCount.toString())
             mCount++;
             if (mShowCount != null)
-//                mShowCount.text = mCount.toString()
-                  model.currentName.setValue(mCount)
+                mShowCount.text = mCount.toString()
         })
 
         buttonToast.setOnClickListener(View.OnClickListener {
